2aa3a86bc4329afadde6305498d5ff11b6944cd3
kvtvs <kata.sara-aho@metropolia.fi>
2022-03-21 12:01:04 +0200

    osa-2

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,8 +1,40 @@
 /* eslint-disable no-unused-vars */
 import PropTypes from 'prop-types';
+import useForm from '../hooks/FormHooks';
 
 const RegisterForm = (props) => {
-  return <div>RegisterForm</div>;
+  const {inputs, handleInputChange} = useForm();
+  console.log(inputs);
+
+  return (
+    <>
+      <form>
+        <input
+          placeholder="username"
+          name="username"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="password"
+          name="password"
+          type="password"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="email"
+          name="email"
+          type="email"
+          onChange={handleInputChange}
+        />
+        <input
+          placeholder="fullname"
+          name="full_name"
+          onChange={handleInputChange}
+        />
+        <input type="submit" value="register" />
+      </form>
+    </>
+  );
 };
 
 RegisterForm.propTypes = {};
